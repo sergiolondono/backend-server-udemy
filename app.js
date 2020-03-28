b03eca41c24d5c45dcf87cbadc4b782af0f48270
Sergio Andres Londono Gallo <Sergio.Londono@carvajal.com>
2020-03-28 18:09:37 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+var app = express();
+
+// Establecer conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) => {
+    if (err) throw err;
+
+    console.log('Database: \x1b[32m%s\x1b[0m', 'online');
+});
+
+// Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente!'
+    });
+});
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server port 3000: \x1b[32m%s\x1b[0m', 'online');
+});
